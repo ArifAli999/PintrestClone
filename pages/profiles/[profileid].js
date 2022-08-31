82b6f0dba3323b6f77e46b1bdfd345bc7ed3afde
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-08-31 16:10:58 +0100

    follow/unfollow inn profiles

--- pages/profiles/[profileid].js
+++ pages/profiles/[profileid].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { collection, query, limit, where, doc } from "firebase/firestore";
+import { collection, query, limit, where, doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from 'react';
 import { app, db } from '../../firebase/firebase.config'
 import { useFirestoreDocument, useFirestoreQueryData } from "@react-query-firebase/firestore";
@@ -10,13 +10,54 @@ import { FaSpinner } from 'react-icons/fa';
 const Post = ({ profileid }) => {
     const router = useRouter()
 
+
+
+    const [followers, setFollowers] = useState([])
+    const [followed, setFollowed] = useState(false)
     const { userDetails } = useAuthStore();
+
+    const { userProfile} = useAuthStore()
+
+
+
+
+    useEffect(() => {
+        const fetchNames = async () => {
+          
+            try {
+                const unsub = onSnapshot(doc(db, "users", profileid), (doc) => {
+                 
+            
+                   setFollowers(doc.data())
+                });
+
+
+
+            } catch (error) {
+                console.log("error", error);
+            }
+
+    
+           
+
+        };
+
+
+
+        fetchNames();
+    }, []);
+    
 
 
     const ref = query(
         collection(db, "posts"),
         where("useruid", "==", profileid),
     );
+
+
+    const followUser =  () => {    
+        followers.FollowedBy && followers.FollowedBy.find(x => x.userid === userProfile.uid) ? console.log('followed') : console.log('not followed')
+    }
 
 
     const ref2 = query(
@@ -57,7 +98,7 @@ const Post = ({ profileid }) => {
     const snapshot = data.data;
     const userlikes = data2.data
     const user = data3.data
-    console.log(user)
+
 
 
 
@@ -77,9 +118,19 @@ const Post = ({ profileid }) => {
                         <h2 className='md:text-4xl text-xl text-black font-bold'>{user && user.map((m)=> m.username)}</h2>
                     </div>
                     <div className='flex gap-2 mb-2'>
-                        <button className="bg-white text-pink-500 border border-pink-400 hover:bg-pink-500 transition-all duration-300 ease-linear hover:text-white rounded  font-bold py-2 px-4 ">
+
+                        {userProfile.uid === user && user.map((m) => m.useruid) ? <button className="bg-white text-pink-500 border border-pink-400 hover:bg-pink-500 transition-all duration-300 ease-linear hover:text-white rounded  font-bold py-2 px-4 ">
                             Edit
-                        </button>
+                        </button> : 
+                        
+                 
+
+                            followers.FollowedBy && followers.FollowedBy.find(x => x.userid === userProfile.uid) ? <button className='text-white bg-pink-500 border border-pink-600 rounded-full p-2  px-4'>Unfollow</button> : <button className='text-pink-600 border border-pink-600 rounded-full p-2 px-4'>Unfollow</button> 
+
+                    
+                         
+                        }
+   
 
 
 
