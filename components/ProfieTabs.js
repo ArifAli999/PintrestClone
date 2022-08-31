d6761d25cafed36dda047999011a552f9b53c4d7
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-08-31 14:13:03 +0100

    user profiles

--- components/ProfieTabs.js
+++ components/ProfieTabs.js
@@ -2,8 +2,13 @@ import { Tab } from '@headlessui/react'
 import PostGrid from './PostGrid'
 import LikedPosts from './LikedPosts'
 import ListPosts from './ListPosts'
+import useAuthStore from '../store/authStore'
 
 function ProfileTabs({ posts, likes, profileid }) {
+
+    const { userProfile } = useAuthStore()
+
+
     return (
         <Tab.Group >
             <Tab.List className='flex space-x-1 irounded-xl items-center justify-center gap-6 p-2 mt-4'>
@@ -16,10 +21,14 @@ function ProfileTabs({ posts, likes, profileid }) {
                 <Tab className={({ selected }) =>
                     selected ? 'bg-pink-500 text-white  py-2 px-4  rounded-full transition-all duration-50 ease-linear ' : 'bg-gray-100 text-black transition-all duration-50 ease-linear py-2 px-4  rounded-full'
                 }>Favourites</Tab>
-                <Tab className={({ selected }) =>
+
+                {userProfile.uid === profileid ? <Tab className={({ selected }) =>
                     selected ? 'bg-pink-500 text-white  py-2 px-4  rounded-full transition-all duration-50 ease-linear ' : 'bg-gray-100 text-black transition-all duration-50 ease-linear py-2 px-4  rounded-full'
-                }>Saved</Tab>
+                }>Saved</Tab> : null}
+             
             </Tab.List>
+
+
             <Tab.Panels>
                 <Tab.Panel>
                     <PostGrid posts={posts} />
@@ -27,9 +36,13 @@ function ProfileTabs({ posts, likes, profileid }) {
                 <Tab.Panel>
                     <LikedPosts likes={likes} />
                 </Tab.Panel>
-                <Tab.Panel className='w-full'>
+
+                {userProfile.uid === profileid ? <Tab.Panel className='w-full'>
                     <ListPosts profileid={profileid} />
-                </Tab.Panel>
+                </Tab.Panel> : null }
+          
+
+
             </Tab.Panels>
         </Tab.Group>
     )
--- pages/profiles/[profileid].js
+++ pages/profiles/[profileid].js
@@ -18,9 +18,15 @@ const Post = ({ profileid }) => {
         where("useruid", "==", profileid),
     );
 
+
     const ref2 = query(
         collection(db, "posts"),
         where("likedBy", 'array-contains', profileid),
+    );
+
+    const ref3= query(
+        collection(db, "users"),
+        where("useruid", "==", profileid),
     );
 
     const data2 = useFirestoreQueryData(["userFavs", profileid], ref2, {
@@ -30,6 +36,12 @@ const Post = ({ profileid }) => {
     const data = useFirestoreQueryData(["userProfile", profileid], ref, {
         subscribe: true,
     });
+
+    const data3 = useFirestoreQueryData(["userDetails", profileid], ref3, {
+        subscribe: true,
+    });
+
+
 
 
 
@@ -44,7 +56,8 @@ const Post = ({ profileid }) => {
 
     const snapshot = data.data;
     const userlikes = data2.data
-    console.log(userlikes)
+    const user = data3.data
+    console.log(user)
 
 
 
@@ -61,7 +74,7 @@ const Post = ({ profileid }) => {
             <div className='flex flex-col'>
                 <div className='mt-5 mb-0 py-2.5 ml-6 mr-6 flex items-center justify-between border-b border-gray-300 '>
                     <div className='flex-1 mb-2'>
-                        <h2 className='md:text-4xl text-xl text-black font-bold'>{userDetails.username}</h2>
+                        <h2 className='md:text-4xl text-xl text-black font-bold'>{user && user.map((m)=> m.username)}</h2>
                     </div>
                     <div className='flex gap-2 mb-2'>
                         <button className="bg-white text-pink-500 border border-pink-400 hover:bg-pink-500 transition-all duration-300 ease-linear hover:text-white rounded  font-bold py-2 px-4 ">
